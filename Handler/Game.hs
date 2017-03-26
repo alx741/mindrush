4e7e08ecd78aa7761354a7ee7b5ff77135a6f69d
Daniel Campoverde [alx741] <alx@sillybytes.net>
2017-03-26 16:42:22 -0500

    Add new questions

--- Handler/Game.hs
+++ Handler/Game.hs
@@ -144,34 +144,172 @@ questions :: [Question]
 questions =
     [ Question
         1
-        "wut 1?"
-        (Answer 1 "wut1 a1")
-        (Answer 2 "wut1 a2")
-        (Answer 3 "wut1 a3")
-        (Answer 4 "wut1 a4")
-        2
+        "¿Cuál es la capital de Ecuador?"
+        (Answer 1 "Cuenca")
+        (Answer 2 "Guayaquil")
+        (Answer 3 "Ambato")
+        (Answer 4 "Quito")
+        4
+
     , Question
         2
-        "wut 2?"
-        (Answer 1 "wut2 a1")
-        (Answer 2 "wut2 a2")
-        (Answer 3 "wut2 a3")
-        (Answer 4 "wut2 a4")
-        2
+        "No tengo trono ni reina, ni nadie que me comprenda, pero sigo siendo el…"
+        (Answer 1 "Rey")
+        (Answer 2 "Papá de los helados")
+        (Answer 3 "Papirruqui")
+        (Answer 4 "Alma de la fiesta")
+        1
+
     , Question
         3
-        "wut 3?"
-        (Answer 1 "wut3 a1")
-        (Answer 2 "wut3 a2")
-        (Answer 3 "wut3 a3")
-        (Answer 4 "wut3 a4")
-        2
+        "¿Cuántas especies de cada animal llevó Moisés según la biblia en el arca antes del diluvio?"
+        (Answer 1 "2 de cada especie.")
+        (Answer 2 "Una pareja de cada especie")
+        (Answer 3 "Ninguno")
+        (Answer 4 "Todos menos los acuáticos")
+        3
+
     , Question
         4
-        "wut 4?"
-        (Answer 1 "wut4 a1")
-        (Answer 2 "wut4 a2")
-        (Answer 3 "wut4 a3")
-        (Answer 4 "wut4 a4")
+        "¿De qué país es el futbolista Cristiano Ronaldo?"
+        (Answer 1 "España")
+        (Answer 2 "Portugal")
+        (Answer 3 "Brasil")
+        (Answer 4 "Polonia")
+        2
+
+    , Question
+        5
+        "¿Cuál es el idioma oficial de Israel?"
+        (Answer 1 "Hebreo")
+        (Answer 2 "Griego")
+        (Answer 3 "Catalán")
+        (Answer 4 "Hindi")
+        1
+
+    , Question
+        6
+        "¿Cual de estos animales es un rumiante?"
+        (Answer 1 "La ardilla")
+        (Answer 2 "El caracol")
+        (Answer 3 "El loro")
+        (Answer 4 "La vaca")
+        4
+
+    , Question
+        7
+        "¿Cuál es el gentilicio de una persona nacida en Bélgica?"
+        (Answer 1 "Belgatario")
+        (Answer 2 "Belga")
+        (Answer 3 "Belganés")
+        (Answer 4 "Belganita")
+        2
+
+    , Question
+        8
+        "Una persona que tiene dos o más parejas está practicando:"
+        (Answer 1 "El polimorfismo")
+        (Answer 2 "El politeísmo")
+        (Answer 3 "La poligamia")
+        (Answer 4 "La poligrafía")
+        3
+
+    , Question
+        9
+        "¿Cuál de estas chicas vive en el bosque con 7 enanitos?"
+        (Answer 1 "Rapunzel")
+        (Answer 2 "Blancanieves")
+        (Answer 3 "La Bella Durmiente")
+        (Answer 4 "La Cenicienta")
+        2
+
+    , Question
+        10
+        "Cuando un grupo de personas marchan una detrás de otra, se dice que marchan en fila:"
+        (Answer 1 "Griega")
+        (Answer 2 "Rusa")
+        (Answer 3 "India")
+        (Answer 4 "China")
+        3
+
+    , Question
+        11
+        "¿Cuál de los siguientes, NO es un elemento químico?"
+        (Answer 1 "Boro")
+        (Answer 2 "Calcio")
+        (Answer 3 "Acero")
+        (Answer 4 "Fósforo")
+        3
+
+    , Question
+        12
+        "El albinismo se presenta por la carencia de:"
+        (Answer 1 "Pigmentación")
+        (Answer 2 "Calcio")
+        (Answer 3 "Oxígeno")
+        (Answer 4 "Vitamina A")
+        1
+
+    , Question
+        13
+        "Uno de los libros de J. K. Rowling es Harry Potter y:"
+        (Answer 1 "El arca perdida")
+        (Answer 2 "El misterio del príncipe")
+        (Answer 3 "La magia negra")
+        (Answer 4 "La orden secreta de la magia")
+        2
+
+    , Question
+        14
+        "Las Vegas es una ciudad estadounidense que queda en:"
+        (Answer 1 "Oklahoma")
+        (Answer 2 "Nevada")
+        (Answer 3 "Arizona")
+        (Answer 4 "Colorado")
+        2
+
+    , Question
+        15
+        "¿Que clase de palabra es \"según\"?"
+        (Answer 1 "Verbo")
+        (Answer 2 "Preposición")
+        (Answer 3 "Adjetivo")
+        (Answer 4 "Sustantivo")
+        3
+
+    , Question
+        16
+        "¿Cuál de los siguientes países limita con Francia?"
+        (Answer 1 "Croacia")
+        (Answer 2 "Alemania")
+        (Answer 3 "Portugal")
+        (Answer 4 "Turquía")
+        2
+
+    , Question
+        17
+        "En la mitología griega, Eros es considerado el dios del:"
+        (Answer 1 "Guerra")
+        (Answer 2 "Tierra")
+        (Answer 3 "Amor")
+        (Answer 4 "Mar")
+        3
+
+    , Question
+        18
+        "La jibia es un:"
+        (Answer 1 "Insecto")
+        (Answer 2 "Reptil")
+        (Answer 3 "Molusco")
+        (Answer 4 "Pez")
+        1
+
+    , Question
+        19
+        "La coroides es una membrana que se encuentra en:"
+        (Answer 1 "El corazón")
+        (Answer 2 "El ojo")
+        (Answer 3 "El pulmón")
+        (Answer 4 "El cerebro")
         2
     ]
